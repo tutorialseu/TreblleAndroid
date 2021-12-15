302902db9d3fb68e58ab6e031c4608878308cbfa
kulloveth <kulloveth@gmail.com>
2021-12-15 08:20:20 +0100

    update register model

--- app/src/main/java/eu/tutorials/authenticationwithtreblle/data/User.kt
+++ app/src/main/java/eu/tutorials/authenticationwithtreblle/data/User.kt
@@ -7,5 +7,5 @@ package eu.tutorials.authenticationwithtreblle.data
 data class RegisterUser(
     val email: String,
     val password: String,
-    val confirmPassword: String
+    val password_confirmation: String
 )
