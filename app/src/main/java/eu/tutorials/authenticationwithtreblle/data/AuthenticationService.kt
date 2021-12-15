e9e2bfb71ec33759850abcf2a718066def15cd41
kulloveth <kulloveth@gmail.com>
2021-12-15 08:19:04 +0100

    update register model

--- app/src/main/java/eu/tutorials/authenticationwithtreblle/data/AuthenticationService.kt
+++ app/src/main/java/eu/tutorials/authenticationwithtreblle/data/AuthenticationService.kt
@@ -1,11 +1,11 @@
 package eu.tutorials.authenticationwithtreblle.data
 
-import eu.tutorials.authenticationwithtreblle.data.RegisterUser
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthenticationService {
 
-    @POST("Account/Register")
+    //Todo 25: add the api path
+    @POST("${Api.API_PATH}Account/Register")
     suspend fun registerUser(@Body registerUser: RegisterUser)
 }
--- app/src/main/java/eu/tutorials/authenticationwithtreblle/ui/screens/SignUp.kt
+++ app/src/main/java/eu/tutorials/authenticationwithtreblle/ui/screens/SignUp.kt
@@ -90,7 +90,7 @@ fun SignUp(viewModel: MainViewModel) {
                    registerUser from viewModel and pass in registerUser
                 * */
                 val registerUser = RegisterUser(email = emailState.value,password = passwordState.value,
-                    confirmPassword = passwordState.value)
+                    password_confirmation = passwordState.value)
                 viewModel.registerUser(registerUser = registerUser)
             }, modifier = Modifier
                 .fillMaxWidth()
