9d1b5df461d2a1cd1b4cddf21bf7a6e0832952bc
kulloveth <kulloveth@gmail.com>
2021-12-15 08:09:39 +0100

    update base url

--- app/src/main/java/eu/tutorials/authenticationwithtreblle/data/Api.kt
+++ app/src/main/java/eu/tutorials/authenticationwithtreblle/data/Api.kt
@@ -9,32 +9,37 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 //Todo 6: Create a singleton class for building Retrofit,Moshi and logging interceptor
 object Api {
   //Todo 7: create the base url which is the consistent part of the api address
-    private val BASE_URL = "https://authteblle20211206115422.azurewebsites.net/api/"
+    private val BASE_URL = "https://treblledemo20211214131915.azurewebsites.net/"
 
-    //Todo 8: create the Moshi converter with KotlinJsonAdapterFactory for kotlin specific conversions
+    /*Todo 8: We create this path as a variable because it will be consistent and reused across 3
+       of the 4 endpoints we will connecting to
+     */
+    const val API_PATH = "api/"
+
+    //Todo 9: create the Moshi converter with KotlinJsonAdapterFactory for kotlin specific conversions
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
 
-    //Todo 9: create the logging interceptor to set up logging for the status when requests are been made
+    //Todo 10: create the logging interceptor to set up logging for the status when requests are been made
     private val logging = HttpLoggingInterceptor()
 
 
-    //Todo 10 Add the logging interceptor to OKHttpClient
+    //Todo 11 Add the logging interceptor to OKHttpClient
     private val httpClient = OkHttpClient.Builder().apply {
         logging.level = HttpLoggingInterceptor.Level.BODY
         addNetworkInterceptor(logging)
 
     }.build()
 
-  //Todo 11: initialise retrofit and add the moshi converter, nase url and httpClient
+  //Todo 12: initialise retrofit and add the moshi converter, nase url and httpClient
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .client(httpClient)
         .build()
 
-    //Todo 12: create the service class using the retrofit library
+    //Todo 13: create the service class using the retrofit library
     val authService: AuthenticationService by lazy { retrofit.create(AuthenticationService::class.java) }
 }
